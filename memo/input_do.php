f5cb61b98bb59a8777f748e57de3e69191a20ae7
livtecq <nishiki1202@gmail.com>
2021-11-22 07:54:36 +0900

    フォームと連携したDB操作

---
+++ memo/input_do.php
@@ -0,0 +1,22 @@
+<?php
+// データベースを壊す文字列などの危険性がある
+// $memo = $_POST['memo'];
+
+$memo = filter_input(INPUT_POST, 'memo', FILTER_SANITIZE_SPECIAL_CHARS);
+
+$db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+$stmt = $db->prepare('insert into memos(memo) values(?)');
+if (!$stmt):
+    die($db->error);
+endif;
+
+$stmt->bind_param('s', $memo);
+$ret = $stmt->execute();
+
+if ($ret):
+    echo '登録されました';
+else:
+    echo $db->error;
+endif;
+
+?>
