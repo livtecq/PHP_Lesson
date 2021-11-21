0e946a8f2a663425b878b98cc678071956a561f2
livtecq <nishiki1202@gmail.com>
2021-11-22 07:04:51 +0900

    別名を使ってPHPでCOUNTの結果を表示する

--- lesson03/sample02.php
+++ lesson03/sample02.php
@@ -9,22 +9,39 @@
 <body>
     <!-- PHPでSELECT構文を実行する -->
     <?php
-        $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-        $records = $db->query('select * from my_items');
-        if($records) {
-            while ($record = $records->fetch_assoc()) {
-                // カラム名大文字小文字は正しく入力しないとエラーになる。(['ITEM_NAME'] ['item_name'])
-                echo $record['ITEM_NAME'] . '<br>';
-            }
-        } else {
-            echo $db->error;
-        }
+        // $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+        // $records = $db->query('select * from my_items');
+        // if($records) {
+        //     while ($record = $records->fetch_assoc()) {
+        //         // カラム名大文字小文字は正しく入力しないとエラーになる。(['ITEM_NAME'] ['item_name'])
+        //         echo $record['ITEM_NAME'] . '<br>';
+        //     }
+        // } else {
+        //     echo $db->error;
+        // }
 
         // 変数の中身を確認する
         // var_dump($records);
 
         // echoでは正しく表示されない。変数の中身がObjectのため。
         // echo $records;
+
+    ?>
+
+    <!-- 別名を使ってPHPでCOUNTの結果を表示する -->
+    <?php
+        $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
+        
+        // 別名は as で変更する。
+        $records = $db->query('select count(*) as cnt from my_items');
+        if($records) {
+            while ($record = $records->fetch_assoc()) {
+                // カラム名大文字小文字は正しく入力しないとエラーになる。(['ITEM_NAME'] ['item_name'])
+                echo $record['cnt'] . '<br>';
+            }
+        } else {
+            echo $db->error;
+        }
 
     ?>
 
