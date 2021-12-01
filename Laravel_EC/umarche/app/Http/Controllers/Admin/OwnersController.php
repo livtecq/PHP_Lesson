ad3e11cd954aa7497f3aa4a2da2e1cb4fe31e4cc
livtecq <nishiki1202@gmail.com>
2021-12-01 12:57:51 +0900

    index, create

--- Laravel_EC/umarche/app/Http/Controllers/Admin/OwnersController.php
+++ Laravel_EC/umarche/app/Http/Controllers/Admin/OwnersController.php
@@ -20,14 +20,14 @@ class OwnersController extends Controller
     public function index()
     {
         // Carbonの機能
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now->year;
-        echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now->year;
+        // echo $date_parse;
 
         //
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -37,9 +37,13 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get, $q_first, $c_test);
+
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+
         return view(
             'admin.owners.index',
-            compact('e_all', 'q_get')
+            compact('owners')
         );
     }
 
@@ -51,6 +55,7 @@ class OwnersController extends Controller
     public function create()
     {
         //
+        return view('admin.owners.create');
     }
 
     /**
