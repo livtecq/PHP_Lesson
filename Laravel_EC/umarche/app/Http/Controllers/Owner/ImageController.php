f445c8896c9d5a79f588dce096c5d8bfe322e4d9
livtecq <nishiki1202@gmail.com>
2021-12-03 04:24:18 +0900

    image, create, validation

--- Laravel_EC/umarche/app/Http/Controllers/Owner/ImageController.php
+++ Laravel_EC/umarche/app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UploadImageRequest;
+
 
 
 class ImageController extends Controller
@@ -52,6 +54,7 @@ class ImageController extends Controller
     public function create()
     {
         //
+        return view('owner.images.create');
     }
 
     /**
@@ -60,9 +63,10 @@ class ImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UploadImageRequest $request)
     {
         //
+        dd($request);
     }
 
     /**
