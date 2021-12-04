763f1caf72b0cd05b5252810a7b0b0ceb8984424
livtecq <nishiki1202@gmail.com>
2021-12-04 10:36:50 +0900

    constant

--- Laravel_EC/umarche/app/Http/Controllers/Owner/ProductController.php
+++ Laravel_EC/umarche/app/Http/Controllers/Owner/ProductController.php
@@ -214,10 +214,10 @@ class ProductController extends Controller
                     $product->is_selling = $request->is_selling;
                     $product->save();
 
-                    if ($request->type === '1') {
+                    if ($request->type === \Constant::PRODUCT_LIST['add']) { // use文で読み込むか、\Constant::PRODUCT_LIST[''] で読み込む
                         $newQuantity = $request->quantity;
                     }
-                    if ($request->type === '2') {
+                    if ($request->type === \Constant::PRODUCT_LIST['reduce']) {
                         $newQuantity = $request->quantity * -1;
                     }
                     Stock::create([
